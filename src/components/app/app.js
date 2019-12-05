17f15223f6f82a9e3d7719ffeede9b72813db816
FireRipper <shabelnyk.ilya@gmail.com>
2019-12-05 15:20:45 +0200

    feat: add - reducer, action, store, 2 pages (Home, Card), test data in bookstore-service.js, routes in App. Change src/index.js (add provider, processing error, access to bookstoreService and Routing (BrowserRouter))

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { CardPage, HomePage } from '../pages'
 
-const App = () => (
-    <div>
-        I
-    </div>
-)
+const App = () => {
+    return (
+        <div>
+            <Switch>
+                <Route path='/' exact component={HomePage} />
+                <Route path='/card/' component={CardPage} />
+                <Route />
+            </Switch>
+        </div>)
+}
 
 export default App
---
+++ src/components/pages/card-page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardPage = () => (
+  <div>
+      Card page
+  </div>
+)
+
+export default CardPage
