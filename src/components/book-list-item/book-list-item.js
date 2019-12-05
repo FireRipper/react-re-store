d6e7773961cba9566fb6ab85ea933967ef0c9b90
FireRipper <shabelnyk.ilya@gmail.com>
2019-12-05 16:25:54 +0200

    feat: add functions for get data from store and add utils(compose)

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+
 import './book-list-item.css'
 
 const BookListItem = ({ book }) => {
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,22 +2,9 @@ import React from 'react'
 import BookList from '../book-list'
 
 const HomePage = () => {
-    const _books = [
-        {
-            id: 1,
-            title: 'Production-Ready Microservices',
-            author: 'Susan J. Fowler'
-        },
-        {
-            id: 2,
-            title: 'Release IT',
-            author: 'Micheal T. Nygard'
-        }
-    ]
-
     return (
         <div>
-            <BookList books={_books} />
+            <BookList />
         </div>
     )}
 
