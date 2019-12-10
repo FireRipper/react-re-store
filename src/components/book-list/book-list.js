ae675a667e5e5b4aeed1fb7929dd06c4fa80c707
FireRipper <shabelnyk.ilya@gmail.com>
2019-12-10 12:33:51 +0200

    feat(processing-error): add action (BOOKS_ERROR), Its for processing error, when books have not received

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,31 +3,36 @@ import BookListItem from '../book-list-item'
 import { connect } from 'react-redux'
 
 import { withBookstoreService } from '../hoc'
-import { booksLoaded, booksRequested } from '../../actions'
+import { booksLoaded, booksRequested, booksError } from '../../actions'
 import { compose } from '../../utils'
 
 import Spinner from '../spinner'
 import './book-list.css'
+import ErrorIndicator from '../error-indicator'
 
 class BookList extends Component {
 
     componentDidMount() {
         // 1, - receive data
         // 2. - dispatch action to store
-        const { bookstoreService, booksLoaded, booksRequested } = this.props
+        const { bookstoreService, booksError,
+            booksLoaded, booksRequested } = this.props
 
         booksRequested()
         bookstoreService.getBooks()
-            .then((data) => {
-                booksLoaded(data)
-            })
+            .then((data) => {booksLoaded(data)})
+            .catch((err) => booksError(err))
     }
 
     render() {
-        const { books, loading } = this.props
+        const { books, loading, error } = this.props
 
         if (loading) {
             return <Spinner />
+        }
+
+        if(error) {
+            return <ErrorIndicator />
         }
 
         return (
@@ -45,13 +50,14 @@ class BookList extends Component {
     }
 }
 
-const mapStateToProps = ({ books, loading }) => {
-    return { books, loading }
+const mapStateToProps = ({ books, loading, error }) => {
+    return { books, loading, error }
 }
 
 const mapDispatchToProps = {
     booksLoaded,
-    booksRequested
+    booksRequested,
+    booksError
 }
 
 export default compose(
