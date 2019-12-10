0c043ad1c831db8c40c5b8317be03fcf125a56a0
FireRipper <shabelnyk.ilya@gmail.com>
2019-12-10 12:16:22 +0200

    fix(spinner): add - action (BOOKS_REQUESTED), loading when user goes to page Home, display loader instead books,

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,18 +3,20 @@ import BookListItem from '../book-list-item'
 import { connect } from 'react-redux'
 
 import { withBookstoreService } from '../hoc'
-import { booksLoaded } from '../../actions'
+import { booksLoaded, booksRequested } from '../../actions'
 import { compose } from '../../utils'
 
-import './book-list.css'
 import Spinner from '../spinner'
+import './book-list.css'
 
 class BookList extends Component {
 
     componentDidMount() {
         // 1, - receive data
         // 2. - dispatch action to store
-        const { bookstoreService, booksLoaded } = this.props
+        const { bookstoreService, booksLoaded, booksRequested } = this.props
+
+        booksRequested()
         bookstoreService.getBooks()
             .then((data) => {
                 booksLoaded(data)
@@ -48,7 +50,8 @@ const mapStateToProps = ({ books, loading }) => {
 }
 
 const mapDispatchToProps = {
-    booksLoaded
+    booksLoaded,
+    booksRequested
 }
 
 export default compose(
