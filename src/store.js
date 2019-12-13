8903951805804b72a6e942d9843e323fbafffd58
FireRipper <shabelnyk.ilya@gmail.com>
2019-12-13 23:50:17 +0200

    feat(store): add middleware for detecting all action (action.type) and display in console these are actions, add middleware for check what type of actions was called (sting or another type)

--- src/store.js
+++ src/store.js
@@ -1,6 +1,58 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const logMiddleware = ({ getState }) => (dispatch) => (action) => {
+    console.log(action.type, getState())
+    return dispatch(action)
+}
+
+const stringMiddleware = () => (next) => (action) => {
+    if(typeof action ===  'string') {
+        return next({
+            type:action
+        })
+    }
+    return next(action)
+}
+
+const store = createStore(reducer,
+    applyMiddleware(
+        stringMiddleware,
+        logMiddleware
+    )
+)
+
+store.dispatch('HELLO_WORLD')
 
 export default store
+
+/*const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args)
+    const originalDispatch = store.dispatch
+    store.dispatch = (action) => {
+
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            })
+        }
+
+        return originalDispatch(action)
+    }
+
+    return store
+}*/
+
+/*const logEnhancer = (createStore) => (...args) => {
+    /!*-----------------------(store) -----------------------*!/
+    const store = createStore(...args)
+    /!*----------------(dispatch) ---------------------*!/
+    const originalDispatch = store.dispatch
+    /!*-----------------------(action) --------------------*!/
+    store.dispatch = (action) => {
+        console.log(action.type)
+        return originalDispatch(action)
+    }
+    return store
+}*/
